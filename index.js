f3a9ace0c145c8d5f72ce8dcf88dc9216525cd7c
k33g <ph.charriere@gmail.com>
2017-01-22 11:14:52 +0100

    🐼 added 1st version 🚀

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Promise = require('promise');
+const fetch = require('node-fetch');
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static('public'));
+
+app.listen(port);
+console.log(`🌍 Web Application is started - listening on ${port}`);
