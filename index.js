7a93dbd95bab2fae86c1f5e7830a746125448a9e
busterbunny69 <buster.bunny.69@gmail.com>
2017-01-22 13:25:02 +0000

    🌼 gardening

--- index.js
+++ index.js
@@ -28,18 +28,8 @@ app.get('/sensors/babs-sensor', (req, res) => {
 
 });
 
-app.get('/sensors/buster-sensor', (req, res) => {
+require('./buster-service')(app)
 
-  fetch('http://snow-demo-buster.cleverapps.io/sensors/buster-sensor', {
-    method: 'GET',
-    headers: {
-      "Content-Type": "application/json",
-    }
-  })
-  .then(response => response.json())
-  .then(jsonData => res.send(jsonData));
-
-});
 
 app.listen(port);
 console.log(`🌍 Web Application is started - listening on ${port}`);
